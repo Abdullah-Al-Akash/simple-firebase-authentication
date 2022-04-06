8609adcc72a1a9ac2d490ab063b63145fb6f4327
Abdullah Al Akash <aabdullahalakash@gmail.com>
2022-04-06 22:56:21 +0600

    Successfully Implement Firebase Authentication

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+        apiKey: "AIzaSyArHLmsKDBPvbg5hvlOuNAACnykUw-x0kE",
+        authDomain: "simple-firebase-authenti-c6bf2.firebaseapp.com",
+        projectId: "simple-firebase-authenti-c6bf2",
+        storageBucket: "simple-firebase-authenti-c6bf2.appspot.com",
+        messagingSenderId: "178145318825",
+        appId: "1:178145318825:web:5fd9015d934c2192d856b8"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
